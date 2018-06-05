d31e10b245dcbadafa6ebfc51adb19d370bf6149
IPMaddBuddha <37713898+IanPriceMB@users.noreply.github.com>
2018-06-05 13:29:57 -0600

    finished

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,6 +9,36 @@ module.exports = function(app){
     app.post("/api/friends", function(req, res) {
         var newFriend = req.body;
         friends.push(newFriend);
-        res.json(friends);
+
+        
+        var compatabilityArr = [];
+        var checkerArr = [];
+        var bestFriend;
+        function compatability() {
+            for (let i =0; i < friends.length; i ++){
+                var value = 0;
+                if (newFriend.name !== friends[i].name){
+                    for (let j = 0; j < newFriend.scores.length; j++){
+                        console.log(parseInt(newFriend.scores[j]))
+                        console.log(parseInt(friends[i].scores[j]))
+                        value += Math.abs((parseInt(newFriend.scores[j])) - (parseInt(friends[i].scores[j])))
+                    }
+                    compatabilityArr.push({friendScore: value, friendName: friends[i].name})
+                }
+            }
+            for (let x = 0; x < compatabilityArr.length; x++){
+                checkerArr.push(compatabilityArr[x].friendScore)
+            }
+            console.log(checkerArr)
+            var checker = Math.min.apply(null, checkerArr)
+            console.log(checker)
+            for (let k = 0; k < compatabilityArr.length; k++){
+                if (checker === compatabilityArr[k].friendScore){
+                    bestFriend = {Name: compatabilityArr[k].friendName, FriendValue: checker};
+                }
+            }
+        }
+        compatability();
+        res.json(bestFriend)
     });
 }
